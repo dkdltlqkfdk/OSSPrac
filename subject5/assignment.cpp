3d9ae85212b5ee0a1120989f7db73c416574ce97
dkdltlqkfdlk <yyelin718@gmail.com>
2026-04-09 13:46:46 +0900

    2026-04-09 개인과제5 4번

--- subject5/assignment.cpp
+++ subject5/assignment.cpp
@@ -2,6 +2,7 @@
 struct student{
     char name[10];
     char grade[10];
+    char activation[10];
 };
 int main(){
     student yun;
@@ -9,10 +10,13 @@ int main(){
     std::cin>>yun.name;
     std::cout<<"학번을 입력하세요: ";
     std::cin>>yun.grade;
+    std::cout<<"학과를 입력하세요: ";
+    std::cin>>yun.activation;
     std::cout<<"\n\n";
-    
+
     std::cout<<"<출력>"<<std::endl;
     std::cout<<"이름: "<<yun.name<<std::endl;
     std::cout<<"학번: "<<yun.grade<<std::endl;
+    std::cout<<"학과: "<<yun.activation<<std::endl;
     return 0;
 }
